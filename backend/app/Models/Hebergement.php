1845769e6822cae2339d52858881409a028cfda7
RayaneTks <rayanetks@gmail.com>
2026-02-17 09:44:55 +0100

    fix(backend): safe db update migrations and installation workflow

--- backend/app/Models/Hebergement.php
+++ backend/app/Models/Hebergement.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Hebergement extends Model
 {
-    //
+    public function voyage(): BelongsTo
+    {
+        return $this->belongsTo(Voyage::class);
+    }
 }
--- backend/app/Models/Transport.php
+++ backend/app/Models/Transport.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transport extends Model
 {
-    //
+    public function voyage(): BelongsTo
+    {
+        return $this->belongsTo(Voyage::class);
+    }
 }
